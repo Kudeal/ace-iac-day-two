e51d55a823cbdbe10221e4823484cefbcf5b51ed
Kudeal <46075467+Kudeal@users.noreply.github.com>
2025-01-07 11:53:48 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-     organization = "kenny-aviatrix-iac"
+    organization = "kenny-aviatrix-iac"
     workspaces {
       name = "ace-iac-day-two"
     }
